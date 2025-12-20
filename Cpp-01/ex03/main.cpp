261738937452cc5c4819a7a126d354757a172ce7
DKOK01 <aymansadik2005@gmail.com>
2025-12-20 17:56:13 +0100

    Implement HumanA and HumanB classes with weapon handling and update Makefile

---
+++ Cpp-01/ex03/main.cpp
@@ -0,0 +1,47 @@
+#include "Weapon.hpp"
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int main()
+{
+	Weapon club("crude spiked club");
+
+	HumanA bob("Bob", club);  // Weapon passed to constructor
+	bob.attack();
+
+	club.setType("some other type of club");  // Change weapon type
+	bob.attack();  // Bob sees the change! (reference to same weapon)
+
+
+
+	Weapon club("crude spiked club");
+
+	HumanB jim("Jim");      // No weapon yet
+	jim.setWeapon(club);    // Give weapon later
+	jim.attack();
+
+	club.setType("some other type of club");  // Change weapon type
+	jim.attack();  // Jim sees the change! (pointer to same weapon)
+
+	return 0;
+}
+
+/*
+ * KEY LESSON: When to use REFERENCE vs POINTER
+ *
+ * Use REFERENCE when:
+ * - The object MUST exist (cannot be NULL)
+ * - Must be set at construction time
+ * - Will always refer to the same object
+ * - Example: HumanA always has a weapon
+ *
+ * Use POINTER when:
+ * - The object is OPTIONAL (can be NULL)
+ * - Can be set/changed later
+ * - May point to different objects over time
+ * - Example: HumanB may or may not have a weapon
+ *
+ * WHY DOES CHANGING club AFFECT BOTH BOB AND JIM?
+ * Because both use reference/pointer to the SAME weapon object!
+ * They don't have a COPY - they have access to the ORIGINAL.
+ */
