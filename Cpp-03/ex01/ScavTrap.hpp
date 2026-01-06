dcb99658b0f377978f4ea035789124346cd87eba
DKOOK01 <aymansadik2005@gmail.com>
2026-01-06 14:05:31 +0100

    .

--- Cpp-03/ex01/ScavTrap.hpp
+++ Cpp-03/ex01/ScavTrap.hpp
@@ -4,18 +4,12 @@
 #include "ClapTrap.hpp"
 
 class ScavTrap : public ClapTrap {
-private:
-    std::string _name;
 public:
 	ScavTrap();
 	ScavTrap(const std::string& name);
 	ScavTrap(const ScavTrap& other);
 	ScavTrap& operator=(const ScavTrap& other);
 	~ScavTrap();
-    void setName(std::string name) {
-        _name = name;
-        std::cout << "ScavTrap name set to " << _name << std::endl;
-    };
 
 	void attack(const std::string& target);
 	void guardGate();
